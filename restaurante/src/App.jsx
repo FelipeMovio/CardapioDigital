a383d94b1008f451ca55916f3aeaf20ab5a8641a
FelipeMovio <felipelsmovio@gmail.com>
2025-10-16 14:47:52 -0300

    projeto finalizado

--- restaurante/src/App.jsx
+++ restaurante/src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import ImgRestaurante from "./assets/hashtaurante.webp";
+import Cards from "./components/Cards";
+import Navegacao from "./components/Navegacao";
 
 function App() {
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0);
+
   return (
-    <div>
-      <h1>Cardápio Digital</h1>
-      <p>Bem-vindo ao nosso restaurante!</p>
-    </div>
+    <>
+      <div className="container">
+        <div className="banner">
+          <img src={ImgRestaurante} alt="Imagem do Hashtaurante" />
+        </div>
+
+        <Navegacao
+          categoriaSelecionada={categoriaSelecionada}
+          setCategoriaSelecionada={setCategoriaSelecionada}
+        />
+
+        <Cards categoriaSelecionada={categoriaSelecionada} />
+      </div>
+    </>
   );
 }
 
